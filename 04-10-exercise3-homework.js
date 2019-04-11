b5c5e0c790017762f9bf2013f76e5c9913a2bf66
tsfromm <48682631+tsfromm@users.noreply.github.com>
2019-04-11 09:03:04 -0400

    April 10 HW

---
+++ 04-10-exercise3-homework.js
@@ -0,0 +1,20 @@
+//Let's create a simple todo application
+//Show an unordered list of todo's
+const button = document.querySelector('button')
+const list = document.querySelector('ul')
+
+button.addEventListener('click', function(){ //Add To Do Button
+    let input = document.querySelector('input').value //Retrive Input Box Value
+    list.appendChild(document.createElement('li')) //Create New List item 
+    let newItem = list.children[list.children.length-1]
+    newItem.innerHTML = input //Add Text Input to New List Item 
+    
+    
+    newItem.addEventListener('click', function (){ //New List Item Responds to Click...
+        newItem.setAttribute('style','text-decoration:line-through') //By Getting a Line Through It...
+        setTimeout(function(){ //And After a Second...
+            newItem.remove() //It Gets Deleted!
+        },1000)
+    })
+    document.querySelector('form').reset() //Clear The Text Box For The next Item
+})
